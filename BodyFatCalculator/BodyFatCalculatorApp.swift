ecd161e6a5929c0223184151e3931808b0d7887c
Oncu <oohancan@gmail.com>
2025-05-22 17:01:03 +0300

    Initial Commit

---
+++ BodyFatCalculator/BodyFatCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyFatCalculatorApp.swift
+//  BodyFatCalculator
+//
+//  Created by Oncu Can on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BodyFatCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
